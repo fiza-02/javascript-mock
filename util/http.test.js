31d113abe69e223783a72525c04fc0563dfd5f45
Mohammed Yasmin <mdyasmin0210@gmail.com>
2024-02-20 11:29:45 +0530

    test-specific mock

--- util/http.test.js
+++ util/http.test.js
@@ -6,6 +6,9 @@ const testResponseData = { testKey: "testData" };
 
 const testFetch = vi.fn((url, options) => {
 	return new Promise((resolve, reject) => {
+		if (typeof options.body !== "string") {
+			return reject("Not a string.");
+		}
 		const testResponse = {
 			ok: true,
 			json() {
@@ -24,4 +27,9 @@ it("should return any available response data", () => {
 	const testData = { key: "test" };
 
 	return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+});
+
+it("should convert the provided data to JSON before sending the request", () => {
+	const testData = { key: "test" };
+	return expect(sendDataRequest(testData)).not.rejects.toBe("Not as string.");
 });
