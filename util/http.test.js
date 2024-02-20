fd3bd2dbe7643b42aa82c08ce2f6c26904c7d825
Mohammed Yasmin <mdyasmin0210@gmail.com>
2024-02-20 12:46:22 +0530

    mocking in selected places - mockImplemntationOnce

--- util/http.test.js
+++ util/http.test.js
@@ -1,6 +1,7 @@
 import { it, vi, expect } from "vitest";
 
 import { sendDataRequest } from "./http";
+import { HttpError } from "./errors";
 
 const testResponseData = { testKey: "testData" };
 
@@ -38,4 +39,26 @@ it("should convert the provided data to JSON before sending the request", async 
 		errorMessage = err;
 	}
 	expect(errorMessage).not.toBe("Not a string.");
+});
+
+it("should throw an HttpError in case of non-ok repsonses", () => {
+	testFetch.mockImplementationOnce((url, options) => {
+		return new Promise((resolve, reject) => {
+			if (typeof options.body !== "string") {
+				return reject("Not a string.");
+			}
+			const testResponse = {
+				ok: false,
+				json() {
+					return new Promise((resolve, reject) => {
+						resolve(testResponseData);
+					});
+				},
+			};
+			resolve(testResponse);
+		});
+	});
+	const testData = { key: "test" };
+
+	return expect(sendDataRequest(testData)).rejects.toBeInstanceOf(HttpError);
 });
